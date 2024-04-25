4ab172c221075bf3b0140f5bd2eb5d6430d20fa8
Tim Savage <tim@savage.company>
2024-04-26 03:43:23 +1000

    Implement a very rough terminal interface.

--- src/devices/builtin/terminal.zig
+++ src/devices/builtin/terminal.zig
@@ -1,12 +1,14 @@
 //! Terminal peripheral device.
 
 const std = @import("std");
+const rl = @import("raylib");
 const Peripheral = @import("../../peripheral.zig");
 const PeripheralError = Peripheral.PeripheralError;
 
 const Self = @This();
 
-const stdout = std.io.getStdOut().writer();
+cursor_pos: usize = 0,
+character_buffer: [24][40:0]u8 = undefined,
 
 pub fn init(allocator: std.mem.Allocator) !*Self {
     const instance = try allocator.create(Self);
@@ -19,11 +21,23 @@ pub fn peripheral(self: *Self) Peripheral {
         .ptr = self,
         .vtable = &.{
             .name = "Terminal",
-            .description = "Simple text terminal.",
+            .description = "Video display text terminal.",
+            .loop = loop,
             .read = read,
             .write = write,
         },
     };
+}
+
+fn loop(ctx: *anyopaque) PeripheralError!void {
+    const self: *Self = @ptrCast(@alignCast(ctx));
+
+    rl.clearBackground(rl.Color.black);
+    for (1.., self.character_buffer) |row_number, line| {
+        const slice = std.mem.sliceTo(&line, 0);
+        const row: i32 = @intCast(row_number);
+        rl.drawText(slice, 0, 480 - (row * 20), 20, rl.Color.green);
+    }
 }
 
 /// Read a value from a peripheral register.
@@ -32,10 +46,28 @@ fn read(_: *anyopaque, _: u16) PeripheralError!u8 {
 }
 
 /// Write a value to a peripheral register.
-fn write(_: *anyopaque, addr: u16, data: u8) PeripheralError!void {
+fn write(ctx: *anyopaque, addr: u16, data: u8) PeripheralError!void {
     switch (addr) {
         0 => {
-            stdout.print("{c}", .{data}) catch return PeripheralError.HardwareFailure;
+            const self: *Self = @ptrCast(@alignCast(ctx));
+
+            if (data == '\n') {
+                // Shift each line up
+                var idx = self.character_buffer.len - 1;
+                while (idx > 0) {
+                    @memcpy(&self.character_buffer[idx], &self.character_buffer[idx - 1]);
+                    idx -= 1;
+                }
+
+                // Clear current line.
+                @memset(&self.character_buffer[0], 0);
+                self.cursor_pos = 0;
+            } else {
+                if (self.cursor_pos < self.character_buffer[0].len) {
+                    self.character_buffer[0][self.cursor_pos] = data;
+                    self.cursor_pos += 1;
+                }
+            }
         },
         else => return PeripheralError.AddressIndex,
     }
