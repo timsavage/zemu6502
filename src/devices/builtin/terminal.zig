40df39fc1f5913ffc34df87dccea32611af82e97
Tim Savage <tim@savage.company>
2024-04-26 18:56:21 +1000

    Make configurable

--- src/devices/builtin/terminal.zig
+++ src/devices/builtin/terminal.zig
@@ -2,17 +2,33 @@
 
 const std = @import("std");
 const rl = @import("raylib");
+const VideoConfig = @import("../../config.zig").VideoConfig;
 const Peripheral = @import("../../peripheral.zig");
 const PeripheralError = Peripheral.PeripheralError;
 
 const Self = @This();
 
+width: i32,
+height: i32,
+scale: i32,
+font_size: i32 = 10,
 cursor_pos: usize = 0,
-character_buffer: [24][40:0]u8 = undefined,
+line_buffer: [24][40:0]u8 = undefined,
 
-pub fn init(allocator: std.mem.Allocator) !*Self {
+pub fn init(allocator: std.mem.Allocator, video_config: *const VideoConfig) !*Self {
     const instance = try allocator.create(Self);
-    instance.* = .{};
+    instance.* = .{
+        .width = video_config.width,
+        .height = video_config.height,
+        .scale = video_config.scale,
+    };
+
+    // Zero all buffers
+    for (0..instance.line_buffer.len) |idx| {
+        const line = &instance.line_buffer[idx];
+        @memset(line, 0);
+    }
+
     return instance;
 }
 
@@ -32,11 +48,14 @@ pub fn peripheral(self: *Self) Peripheral {
 fn loop(ctx: *anyopaque) PeripheralError!void {
     const self: *Self = @ptrCast(@alignCast(ctx));
 
+    const font_size = self.font_size * self.scale;
+    const offset = self.height;
+
     rl.clearBackground(rl.Color.black);
-    for (1.., self.character_buffer) |row_number, line| {
+    for (1.., self.line_buffer) |row_number, line| {
         const slice = std.mem.sliceTo(&line, 0);
         const row: i32 = @intCast(row_number);
-        rl.drawText(slice, 0, 480 - (row * 20), 20, rl.Color.green);
+        rl.drawText(slice, 0, offset - (row * font_size), font_size, rl.Color.green);
     }
 }
 
@@ -51,22 +70,25 @@ fn write(ctx: *anyopaque, addr: u16, data: u8) PeripheralError!void {
         0 => {
             const self: *Self = @ptrCast(@alignCast(ctx));
 
-            if (data == '\n') {
-                // Shift each line up
-                var idx = self.character_buffer.len - 1;
-                while (idx > 0) {
-                    @memcpy(&self.character_buffer[idx], &self.character_buffer[idx - 1]);
-                    idx -= 1;
-                }
+            switch (data) {
+                '\n' => {
+                    // Shift each line up
+                    var idx = self.line_buffer.len - 1;
+                    while (idx > 0) {
+                        @memcpy(&self.line_buffer[idx], &self.line_buffer[idx - 1]);
+                        idx -= 1;
+                    }
 
-                // Clear current line.
-                @memset(&self.character_buffer[0], 0);
-                self.cursor_pos = 0;
-            } else {
-                if (self.cursor_pos < self.character_buffer[0].len) {
-                    self.character_buffer[0][self.cursor_pos] = data;
-                    self.cursor_pos += 1;
-                }
+                    // Clear current line.
+                    @memset(&self.line_buffer[0], 0);
+                    self.cursor_pos = 0;
+                },
+                else => {
+                    if (self.cursor_pos < (self.line_buffer[0].len - 1)) {
+                        self.line_buffer[0][self.cursor_pos] = data;
+                        self.cursor_pos += 1;
+                    }
+                },
             }
         },
         else => return PeripheralError.AddressIndex,
