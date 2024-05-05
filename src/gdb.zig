fc5bf8ac90b1776e40b10339a278acbef82abecd
Tim Savage <tim@savage.company>
2024-05-05 14:16:34 +1000

    Added query command to fetch all peripherals, migration to using start/end packet methods and track changes to the output buffer to generate the checksum.

--- src/gdb.zig
+++ src/gdb.zig
@@ -41,6 +41,28 @@ pub fn deinit(self: *Self) void {
     self.server.deinit();
 }
 
+// Process a Query (q) packet
+fn processQuery(self: *Self, system: *System, packet: []const u8) !void {
+    if (std.mem.eql(u8, packet, "qPeripherals")) {
+        const start = try self.start_packet();
+        for (system.data_bus.peripherals.items) |item| {
+            try self.out.append(item.peripheral.vtable.name);
+            try self.out.append(":");
+            try self.out.append(&utils.hexDigits(@truncate(item.start >> 8)));
+            try self.out.append(&utils.hexDigits(@truncate(item.start)));
+            try self.out.append(":");
+            try self.out.append(&utils.hexDigits(@truncate(item.end >> 8)));
+            try self.out.append(&utils.hexDigits(@truncate(item.end)));
+            try self.out.append(";");
+        }
+        self.out.len -= 1; // Remove last final separator.
+        try self.end_packet(start);
+    } else {
+        std.log.debug("Unknown query: {s}", .{packet});
+        try self.write_packet("");
+    }
+}
+
 fn processPacket(self: *Self, system: *System) !void {
     // Return if there is an incomplete packet
     const start = self.in.find('$', .{}) catch return;
@@ -91,14 +113,21 @@ fn processPacket(self: *Self, system: *System) !void {
             // Read memory
             if (packet.len >= 8) {
                 const addr = try std.fmt.parseInt(u16, packet[1..5], 16);
-                const length = try std.fmt.parseInt(u16, packet[6..], 16);
+                var length = try std.fmt.parseInt(u16, packet[6..], 16);
+
+                // Calculate length with overflow check.
+                const addr_end = std.math.add(u16, addr, length) catch std.math.maxInt(u16);
+                length = addr_end - addr;
+
                 const data = try self.allocator.alloc(u8, length);
                 defer self.allocator.free(data);
                 for (0..length) |idx| {
                     const offset: u16 = @truncate(idx);
                     data[idx] = system.data_bus.read(addr + offset);
                 }
-                try self.write_binary_packet(data);
+                const packet_start = try self.start_packet();
+                try self.out.appendBytes(data);
+                try self.end_packet(packet_start);
             } else {
                 std.log.warn("[GDB] Ignoring bad packet: {s}", .{packet});
             }
@@ -120,9 +149,8 @@ fn processPacket(self: *Self, system: *System) !void {
             system.mpu.halt();
             try self.write_packet("S11"); // 0x11 (17)
         },
-        'r', 'R' => {
-            system.reset();
-        },
+        'r', 'R' => system.reset(),
+        'q' => try self.processQuery(system, packet),
         else => {
             std.log.info("[GDB] Unknown packet: {s}", .{packet});
             try self.write_packet("");
@@ -132,24 +160,21 @@ fn processPacket(self: *Self, system: *System) !void {
     self.in.removeHead(packet_end);
 }
 
-fn write_packet(self: *Self, data: []const u8) !void {
-    const checksum = utils.modulo256Sum(data);
+fn start_packet(self: *Self) !usize {
     try self.out.append("$");
-    try self.out.append(data);
+    return self.out.len;
+}
+
+fn end_packet(self: *Self, start: usize) !void {
+    const checksum = utils.modulo256Sum(self.out.data[start..self.out.len]);
     try self.out.append("#");
     try self.out.append(&utils.hexDigits(checksum));
 }
 
-fn write_binary_packet(self: *Self, data: []const u8) !void {
-    const bytes = try self.allocator.alloc(u8, data.len * 2);
-    defer self.allocator.free(bytes);
-    _ = try std.fmt.bufPrint(bytes, "{}", .{std.fmt.fmtSliceHexUpper(data)});
-
-    const checksum = utils.modulo256Sum(bytes);
-    try self.out.append("$");
-    try self.out.append(bytes);
-    try self.out.append("#");
-    try self.out.append(&utils.hexDigits(checksum));
+fn write_packet(self: *Self, data: []const u8) !void {
+    const start = try self.start_packet();
+    try self.out.append(data);
+    try self.end_packet(start);
 }
 
 /// Check for an incoming connection.
