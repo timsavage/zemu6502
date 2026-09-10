f8047a9207a21bca697ea7897e9201833f5a441f
Tim Savage <tim@savage.company>
2026-09-11 01:18:22 +1000

    Initial debugger code

---
+++ scripts/emu6502/debugger/cli.py
@@ -0,0 +1,19 @@
+import logging
+from pathlib import Path
+
+from pyapp.app import CliApplication
+
+from emu6502.debugger.gdb import GDBTextInterface
+
+app = CliApplication(application_settings="emu6502.debugger.default_settings")
+log = logging.getLogger("gdb-6502")
+
+
+@app.command
+async def target(*, address: str = "::1", port: int = 6502, image_lst: Path = None):
+    """Run the GDB client."""
+
+    interface = GDBTextInterface(address, port)
+    if image_lst:
+        interface.load_image(image_lst)
+    await interface.run()
